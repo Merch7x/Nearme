32cb55b26b58120349f4777e28d75db548f8fc79
Merch7x <aquamaingi@gmail.com>
2024-05-07 14:04:13 +0300

    Create signup and login forms

---
+++ app/auth/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+
+class LoginForm(FlaskForm):
+    """Creates a login form"""
+    email = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+class SignUp(FlaskForm):
+    """Creates a Registration form for new users"""
+    FirstName = StringField('First Name', validators=[DataRequired()])
+    LastName = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[
+                              DataRequired(), EqualTo('password')])
+    Submit = SubmitField('Sign Up')
