75dc5ad905a9eddb2f0f11e9497bb34cadfbc072
Merch7x <aquamaingi@gmail.com>
2024-05-08 14:34:22 +0300

    Add orm configs

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ class Config:
     """Creates configuration variables"""
     SECRET_KEY = os.environ.get('SECRET_KEY')\
         or 'BBLDRIZZY'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://admin:admin@nm-db-1.cb0sgeak46n9.eu-west-2.rds.amazonaws.com/nearmedev'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
